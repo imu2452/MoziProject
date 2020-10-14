046cb8e8cbd30ad956c5a4edeb6e76632984ff00
aki.a <yellow_a2452@yahoo.co.jp>
2020-10-14 16:52:43 +0900

    feature:リッチメニューと関数の紐付け

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const client = new line.Client({ channelAccessToken: process.env.ACCESSTOKEN });
 
 const crypto = require("crypto");
 
+let text = 'メニューから選択してください'
+let message = {
+  type: "text",
+  text,
+};
+
+const depoText = "文字数を入力してください"
+const confText = "残高確認は現在実装中です"
+const caleText = "カレンダーは現在実装中です"
+
 //コントローラクラス
 exports.handler = function (event, context) {
   let body = JSON.parse(event.body);
+  var reply_token = body.events[0].replyToken
+
   let signature = crypto
     .createHmac("sha256", process.env.CHANNELSECRET)
     .update(event.body)
     .digest("base64");
   let checkHeader = (event.headers || {})["X-Line-Signature"];
-  /**
-   * 
-   */
-
   let operation = body.events[0].message.text
 
   if (signature === checkHeader) {
@@ -29,28 +37,35 @@ exports.handler = function (event, context) {
       };
       context.succeed(lambdaResponse);
       // ③接続確認エラーを確認する。
-
     } else {
 
       switch (operation) {
         case '預金':
+          console.log('選択メニュー：預金');
           //「預金」を受け取ったとき預金用の関数を呼び出す
-          deposit(body);
+          text = depoText;
+          message = deposit(text);
           break;
-
         case '残高確認':
+          console.log('選択メニュー：残高確認');
           //「残高確認」を受け取ったときに残高確認用の関数を呼び出す
-          confilmation(body);
+          text = confText;
+          message = confilmation(text);
           break;
 
         case 'ログイン記録カレンダー':
+          console.log('選択メニュー：ログイン記録カレンダー');
           //「ログイン記録」を受け取ったときに記録用カレンダーノブラウザを表示する
-          calender(body);
-
+          text = caleText;
+          message = calender(text);
           break;
 
         default:
+          break;
       }
+
+      rep(reply_token, message);
+
     }
   } else {
     console.log("署名認証エラー");
@@ -58,36 +73,9 @@ exports.handler = function (event, context) {
 
 };
 
-
-//預金用関数
-function deposit(body) {
-  let depText =
-    '入金額を半角数字で入力してください'
-  const depMessage = {
-    type: "text",
-    depText,
-  };
+function rep(reply_token, message) {
   client
-    .replyMessage(body.events[0].replyToken, depMessage)
-
-  let count = body.events[1].message.text
-
-  //預金時の入力文字チェック（半角文字のみ許容）呼び出す
-
-  let depReplyText =
-    `平素よりお世話になっております。
-文字数貯金です。
-只今、
-【` + count + `円】
-の入金を確認しました。
-またのご利用、お待ちしております。`;
-  const depReplyMessage = {
-    type: "text",
-    text,
-  };
-
-  client
-    .replyMessage(body.events[0].replyToken, depReplyMessage)
+    .replyMessage(reply_token, message)
     .then((response) => {
       let lambdaResponse = {
         statusCode: 200,
@@ -97,6 +85,46 @@ function deposit(body) {
       context.succeed(lambdaResponse);
     })
     .catch((err) => console.log(err));
+}
+
+//預金用関数
+function deposit(text) {
+
+  const depMessage = {
+    type: "text",
+    text,
+  };
+
+  return depMessage;
+
+  //預金時の入力文字チェック（半角文字のみ許容）呼び出す
+  /** 
+    let count = body1.events[0].message.text
+  
+    let depReplyText =
+      `平素よりお世話になっております。
+  文字数貯金です。
+  只今、
+  【` + count + `円】
+  の入金を確認しました。
+  またのご利用、お待ちしております。`;
+    const depReplyMessage = {
+      type: "devReplyText",
+      depReplyText,
+    };
+  
+    client
+      .replyMessage(body.events[0].replyToken, depReplyMessage)
+      .then((response) => {
+        let lambdaResponse = {
+          statusCode: 200,
+          headers: { "X-Line-Status": "OK" },
+          body: '{"result":"completed"}',
+        };
+        context.succeed(lambdaResponse);
+      })
+      .catch((err) => console.log(err));
+  */
 
 
 
@@ -109,26 +137,23 @@ function deposit(body) {
 
 
 //残高確認用の関数
-function confilmation(body) {
-  let confilmationRepText = '残高確認は現在実装中です'
+function confilmation(text) {
   const confilmationRepMessage = {
     type: 'text',
     text,
   }
-  client.replyMessage(body.events[0].replyToken, confilmationRepMessage)
-
+  return confilmationRepMessage;
 };
 //データを取得する関数を呼び出す
 
 
 
-function calender(body) {
-  let calenderrepText = 'カレンダーは現在実装中です'
+function calender(text) {
   const calenderRepMessage = {
     type: 'text',
-    calenderrepText,
+    text,
   }
-  client.replyMessage(body.events[0].replyToken, calenderRepMessage)
+  return calenderRepMessage;
 };
 
 //DBに登録する関数　
